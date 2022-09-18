cf6ca8fd80c8d20e082fff542ad7708cda37370b
Planetologist Topping <ptopping@at-n7.msk.21-school.ru>
2022-09-18 20:49:18 +0300

    little progress in raycasting

--- src/cub/render.c
+++ src/cub/render.c
@@ -6,22 +6,68 @@
 /*   By: ptopping <ptopping@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 17:51:37 by ptopping          #+#    #+#             */
-/*   Updated: 2022/09/17 18:32:06 by ptopping         ###   ########.fr       */
+/*   Updated: 2022/09/18 20:18:08 by ptopping         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+int draw_line(int start, int end)
+
 
 int draw_lines(t_data *data)
 {
     //пересчитать какую то хуйню и отрисовать draw_line
 }
 
+int ft_steps(t_data *data)
+{
+    if (data->game->rayDir.x < 0)
+      {
+        data->game->step.x = -1;
+        data->game->sideDist.x = (data->game->pos.x - data->game->mapX) * data->game->deltaDist.x;
+      }
+    else
+      {
+        data->game->stepX = 1;
+        data->game->sideDist.x = (data->game->mapX + 1.0 - data->game->pos.x) * data->game->deltaDist.x;
+      }
+    if (data->game->rayDir.y < 0)
+      {
+        data->game->stepY = -1;
+        data->game->sideDist.y = (data->game->pos.y - data->game->mapY) * data->game->deltaDist.y;
+      }
+    else
+      {
+        data->game->stepY = 1;
+        data->game->sideDist.y = (data->game->mapY + 1.0 - data->game->pos.y) * data->game->deltaDist.y;
+      }
+}
+
+int dda(t_data *data, int x)
+{
+    data->game->camera.x = 2 * x / double(WIDTH) - 1; //x-coordinate in camera space
+    data->game->rayDir.x = data->game->dir.x + data->game->plane.x * data->game->camera.x;
+    data->game->rayDir.y = data->game->dir.y + data->game->plane.y * data->game->camera.x;
+}
+
 int draw_image(t_data *data)
 {
-    steps(data);
-    dda(data,x);//внутри dda рассчитывается необходимое и внутри же вызывается draw_lines
+    int x;
+
+    x = 0;
+    data->image->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	data->image->addr = mlx_get_data_addr(data->image->img, &data->image->bits_per_pixel, &data->image->line_length,
+								&data->image->endian);//достаем адрес изображения
     
+    while (x < WIDTH)
+    {
+        ft_steps(data);
+        dda(data, x);
+        x++;
+    }
+    //внутри dda рассчитывается необходимое и внутри же вызывается draw_lines
+    mlx_put_image_to_window(vars->mlx, vars->win, vars->data->img, 0, 0);
 }
 
 int render(t_data *data)
--- src/cub/start_game.c
+++ src/cub/start_game.c
@@ -6,7 +6,7 @@
 /*   By: ptopping <ptopping@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 20:46:16 by ptopping          #+#    #+#             */
-/*   Updated: 2022/09/17 18:29:18 by ptopping         ###   ########.fr       */
+/*   Updated: 2022/09/18 19:50:41 by ptopping         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int init_game(t_data *data)
 {
     find_pos(data->map);//может в парсере установить сразу?//x and y start position
-    data->dir.x = -1;//initial direction vector
-    data->dir.y = 0;
-    data->plane.x = 0;//the 2d raycaster version of camera plane
-    data->plane.y = 0.66;
+    data->game->dir.x = -1;//initial direction vector
+    data->game->dir.y = 0;
+    data->game->plane.x = 0;//the 2d raycaster version of camera plane
+    data->game->plane.y = 0.66;
+    data->game->pos.x = 22;
+    data->game->pos.y = 12;
     
 }
 
@@ -34,10 +36,7 @@ int start_game(t_data *data)
     data->mlx = mlx_init();//указатель который дальше запихивается в разные функции
     init_game(data);
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "Cub3D");
-	data->image->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
-	data->image->addr = mlx_get_data_addr(data->image->img, &data->image->bits_per_pixel, &data->image->line_length,
-								&data->image->endian);//достаем адрес изображения
-	//draw_image(data);отрисовываю картинку в первый раз
+	draw_image(data);//отрисовываю картинку в первый раз
 	//mlx_loop_hook(data->mlx, render, data);//здесь уже пересчитываю от координат, удаляю прошлое изображение и отрисовываю заново те draw_image в render
     //mlx_hook(raycast->win, 17, 0, ft_final_cleaner, data);
 	mlx_loop(data->mlx);
